debd8036770cbf862357f77ee9a8a3210ff6198f
lucas <lucasbrito775823@gmail.com>
2026-02-27 10:18:44 -0300

    feat: create tasks table migration and model

---
+++ app/Models/Task.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasUuids;
+
+    protected $primaryKey = 'id';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = ['title', 'description', 'completed'];
+
+    protected $casts = [
+        'completed' => 'boolean',
+    ];
+}
