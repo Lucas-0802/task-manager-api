c583aa025c0aca1414cc5e17a909813a67ecb59f
lucas <lucasbrito775823@gmail.com>
2026-02-28 07:38:08 -0300

    feat: add Postman collection download route and JSON file
    - Implemented a new route for downloading the Postman collection.
    - Added the Postman collection JSON file to the public directory.
    - Updated the web.php routes file to include the new download functionality.
    
    style: update .gitignore file permissions
    
    - Changed permissions for multiple .gitignore files in storage directories to 755.
    
    style: add welcome.css for styling
    
    - Created a new CSS file for the welcome page with responsive design and utility classes.

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
+
+Route::get('/postman/download', function () {
+  $file = public_path('TASK MANAGER.postman_collection.json');
+
+  if (!file_exists($file)) {
+    $file = base_path('TASK MANAGER.postman_collection.json');
+  }
+
+  if (file_exists($file)) {
+    return response()->download($file, 'TASK MANAGER.postman_collection.json', [
+      'Content-Type' => 'application/json',
+    ]);
+  }
+
+  return response()->json(['error' => 'Collection file not found'], 404);
+})->name('postman.download');
